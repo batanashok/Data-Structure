7a7203b604ccb90a4d9e80fff7478949af62ab4d
ashok <batan@kali.root>
2020-07-04 10:01:50 +0530

    sorting algorithm bubble and selection sort

---
+++ ArrayRelate/sort/Sort.java
@@ -0,0 +1,59 @@
+package com.sort;
+
+public class Sort {
+	static int[] bubble(int arr[])
+	{
+		for(int i=0;i<arr.length-1;i++)
+		{
+			boolean flag=false; // this for if array is already sorted best case O(n) otherwise O(n^2)
+			for(int j=0;j<arr.length-1-i;j++)
+			{
+				if(arr[j]>arr[j+1]) // for increasing order and for decreasing order < 
+				{
+					int temp = arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+					flag = true;
+				}
+			}
+			if(flag == false)
+			{
+				break;
+			}
+		}
+		return arr;
+	}
+	
+	static int[] selection(int arr[])
+	{
+		for(int i=0;i<arr.length-1;i++)
+		{
+			int min_index = i;
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[min_index]>arr[j])
+				{
+					min_index = j;
+				}
+			}
+			if(min_index != i)
+			{
+				int temp = arr[min_index];
+				arr[min_index]=arr[i];
+				arr[i]=temp;
+				
+			}
+		}
+		return arr;
+	}
+	
+	static void show(int arr[])
+	{
+		System.out.println("Array element after sort is :");
+		for(int i =0;i<arr.length;i++)
+		{
+			System.out.print(arr[i]+ " ");
+		}
+		System.out.println();
+	}
+}
---
+++ ArrayRelate/sort/Test.java
@@ -0,0 +1,16 @@
+package com.sort;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		int arr[]= {10,24,54,76,45,34,675,34,34,223,656,43,9};
+		
+		Sort.bubble(arr);
+		Sort.show(arr);
+		Sort.selection(arr);
+		Sort.show(arr);
+
+	}
+
+}
